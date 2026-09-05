7781150bf025d3babeb44ebea6e2ba04c8d000b7
ewof <marroq@marroq.xyz>
2026-09-04 23:43:07 -0400

    nettraf

--- config.h
+++ config.h
@@ -4,7 +4,7 @@ static const Block blocks[] = {
 	/* {"⌨", "sb-kbselect", 0, 30}, */
 	{"", "cat /tmp/recordingicon 2>/dev/null",	0,	9},
 	//{"",	"sb-tasks",	10,	26},
-	/* {"",	"sb-music",	0,	11}, */
+	{"",	"sb-music",	0,	11},
 	//{"",	"sb-pacpackages",	0,	8},
 	//{"",	"sb-news",		0,	6},
 	/* {"",	"sb-price xmr-btc \"Monero to Bitcoin\" 🔒 25",	9000,	25}, */
@@ -12,13 +12,14 @@ static const Block blocks[] = {
 	/* {"",	"sb-price eth Ethereum 🍸 23",			9000,	23}, */
 	/* {"",	"sb-price btc Bitcoin 💰 21",			9000,	21}, */
 	//{"",	"sb-torrent",	20,	7},
-	/* {"",	"sb-memory",	10,	14}, */
-	/* {"",	"sb-cpu",		10,	18}, */
+	{"",	"sb-memory",	10,	14},
+	{"",	"sb-cpubars",		1,	1},
+	{"",	"sb-cpu",		10,	18},
 	/* {"",	"sb-moonphase",	18000,	17}, */
 	//{"",	"sb-doppler",	0,	13},
 	//{"",	"sb-forecast",	18000,	5},
 	//{"",	"sb-mailbox",	180,	12},
-	//{"",	"sb-nettraf",	1,	16},
+	{"",	"sb-nettraf",	1,	16},
 	{"",	"sb-volume",	0,	10},
 	{"",	"sb-battery",	5,	3},
 	{"",	"sb-clock",	1,	1},
@@ -34,3 +35,4 @@ static char *delim = " ";
 // vim with the following line in your vimrc/init.vim:
 
 // autocmd BufWritePost ~/.local/src/dwmblocks/config.h !cd ~/.local/src/dwmblocks/; sudo make install && { killall -q dwmblocks;setsid dwmblocks & }
+
